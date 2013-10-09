1df8477896223038479b6d09e5dfde1fd8dbf0d3
Neo <neo@codeincognito.com>
2013-10-10 02:52:13 +0530

    Plugin Info index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	Plugin Name: MediaBox
+	Plugin URI: http://codeincognito.com
+	Description: Media Bridge between DropBox & WordPress
+	Author: CodeIncognito
+	Version: 0.1
+	Author URI: http://codeincognito.com
+	Text Domain: media-box
+ */
